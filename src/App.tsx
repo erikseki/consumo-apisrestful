66363bb89b05a6294e642813f64949c5d1cb5bc0
eriseki <erikseki@hotmail.com>
2023-03-02 19:06:54 -0300

    react-router-dom e staleTime do SWR

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import axios from 'axios';
-import { useQuery } from 'react-query'
+import { Route, Routes } from "react-router-dom";
+import { Repo } from "./pages/Repo";
+import { Repos } from "./pages/Repos";
 
-type Repository = {
-  full_name: string;
-  description: string;
-}
-
-
-function App() {
-
-  const {data, isFetching } = useQuery<Repository[]>('repos', async () => {
-    const response = await axios.get('https://api.github.com/users/erikseki/repos')
-
-    return response.data;
-  }) 
-
-
-  return (
-    <ul>
-      { isFetching && <p>Carregando...</p>}
-      {data?.map(repo => {
-        return(
-          <li key={repo.full_name}>
-            <strong>{repo.full_name}</strong>
-            <p>{repo.description}</p>
-          </li>
-        )
-      })}
-    </ul>
+export function App(){
+  return(
+    <Routes>
+      <Route path="/" element={<Repos />} />
+      <Route path="/repos/*" element={<Repo />} />
+    </Routes>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from 'react-query'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
 import { queryClient } from './services/queryClient'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <App />
-    </QueryClientProvider>
-  </React.StrictMode> 
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
